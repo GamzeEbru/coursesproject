3817ed94b1811a3089592474073de8b08d9b3084
Gamze Ebru Altıkulaç <gamzeebrualtikulac@gmail.com>
2023-07-24 22:56:49 +0300

    final state of the project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Courses
+
+Project I made as a starter while learning django
+
+### Add New Teacher and Courses
+
+Test for adding new courses and teachers to the page
+
+### Tags and Categories
+
+The section that filters by category on the courses and course details page
+
+### Tags and Categories
+
+Users can enroll in the course and unenroll. They can see the courses they have registered for on their own account page.
---
+++ coursesproject_con/courses/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Course
+from .models import Category, Tag
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "available")
+    list_filter = ("available",)
+    search_fields = ("name", "description")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
